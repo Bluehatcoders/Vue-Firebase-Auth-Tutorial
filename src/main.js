8d230cfa07d12c6bba2d261d8a738166328fd3eb
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-05-15 18:41:25 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue';
 import router from './router';
 import firebase from 'firebase/app';
 
-var firebaseConfig = {
-    apiKey: 'YouConfigHere',
-    authDomain: 'YouConfigHere',
-    projectId: 'YouConfigHere',
-    storageBucket: 'YouConfigHere',
-    messagingSenderId: 'YouConfigHere',
-    appId: 'YouConfigHere',
+const firebaseConfig = {
+  apiKey: "AIzaSyB_XI44VWLNUspyqMRjbAZAt9vTAb6LsIY",
+  authDomain: "learning-vhxdqt.firebaseapp.com",
+  databaseURL: "https://learning-vhxdqt.firebaseio.com",
+  projectId: "learning-vhxdqt",
+  storageBucket: "learning-vhxdqt.appspot.com",
+  messagingSenderId: "300528074788",
+  appId: "1:300528074788:web:da3b6c990169e2082258b1"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
